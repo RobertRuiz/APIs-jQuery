697c398796262df740fe604c62e32cf09876af46
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-09-09 17:24:45 -0400

    inserting jquerry value into input value

--- app/assets/javascripts/find_definitions.js
+++ app/assets/javascripts/find_definitions.js
@@ -1,22 +1,30 @@
 // Write Javascript here to handle the click for that button/link
 $(document).ready(function() {
+
+
+
+
+
   $(".find-definitions").on("click", function() {
     // Get the term from the form
     var term = $("#term_name").val();
-    console.log("I did it!")
 
     $.getScript({ url: "/definitions/show_def", data: { term: term } })
-
-    // do other work
   })
-})
 
-// function definition(url) {
-//
-//      $.get(url, function (data) {
-//          var temp = data[0]
-//          alert("Data Loaded: " + data);
-//          alert("temp.id = " + temp.id);
-//          alert("temp.name = " + temp.name);
-//      });
-//  }
+
+
+
+
+
+  $(".found-definitions").on("click", "li", function() {
+    var selected_def = $(this).text();
+    console.log(selected_def)
+  })
+
+
+
+
+
+
+})
